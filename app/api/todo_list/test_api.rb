e5d4bbcb25e4f4fc9bf836fa5e3e43a6e1eba2d5
chris <chris.tram@gmail.com>
2015-09-01 19:03:05 -0700

    add tasks update, delete, index, create

--- app/api/todo_list/test_api.rb
+++ app/api/todo_list/test_api.rb
@@ -16,14 +16,50 @@ module TodoList
 
       get '/reminders' do
         @reminders = Reminder.all
-
         present(@reminders)
       end
 
       post '/reminders' do
-        debugger
         @reminder = Reminder.new(name: params[:name])
         @reminder.save
+      end
+
+      get '/reminders/:id' do
+        @reminder = Reminder.find_by(id: params[:id])
+        present(@reminder, root: 'reminder')
+      end
+
+      get '/reminders/:id/tasks' do
+        @reminder = Reminder.find_by(id: params[:id])
+        @tasks = @reminder.tasks
+        present(@tasks, root: 'tasks')
+      end
+
+
+      get '/tasks' do
+        @tasks = Task.all
+        present(@tasks)
+      end
+
+      post '/tasks' do
+        debugger
+        @task = Task.new(content: params[:content], reminder_id: params[:reminder_id], pending: true)
+        @task.save
+      end
+
+      patch '/tasks/:id' do
+        debugger
+        @task = Task.find(params[:id])
+        params.each do |k, v|
+          unless k == :id
+            @task[k] = v
+          end
+        end
+        @task.save
+      end
+
+      delete '/tasks/:id' do
+        Task.delete(params[:id])
       end
 
 
